f6ff396a628a9bd5642641fd9a356e6b83825c50
SenpaiOfficial <senpai.makeyev@gmail.com>
2020-06-05 22:52:51 +0600

    feat: complete web-site

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
-const {src, dest, watch} = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
+const tiny = require('gulp-tinypng-nokey');
+
 
 
 // Static server
@@ -28,4 +34,50 @@ function serveSass() {
         .pipe(browserSync.stream());
 };
 
+function buildJS(done) {
+    src(['js/**.js', '!js/**.min.js'])
+        .pipe(minify({
+            ext: {
+                min: '.js'
+            },
+        }))
+        .pipe(dest('dist/js/'));
+
+    src('js/**.min.js ').pipe(dest('dist/js/'));
+    done();
+}
+function buildCSS(done) {
+    src('css/**/**.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(dest('dist/css/'));
+    done();
+}
+
+function html(done) {
+    src('**.html').pipe(htmlmin({ collapseWhitespace: true })).pipe(dest('dist/'));
+    done();
+}
+
+function php(done) {
+    src(['**.php'])
+        .pipe(dest('dist/'));
+    src('phpmailer/**/**')
+        .pipe(dest('dist/phpmailer/'));
+    done();
+}
+
+function fonts(done) {
+    src('fonts/**/**')
+        .pipe(dest('dist/fonts/'));
+    done();
+}
+
+function imagemin(done) {
+    src('img/**/**')
+        .pipe(tiny())
+        .pipe(dest('dist/img'));
+    done();
+}
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imagemin);
