f312c2717882f0f8251750baf96acfd405ac419a
SenpaiOfficial <senpai.makeyev@gmail.com>
2020-06-06 17:20:38 +0600

    feat: add video from Youtube in index.html

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function(event) {
- const modal = document.querySelector('.modal');
- const modalBtn = document.querySelectorAll('[data-toggle=modal]');
- const closeBtn = document.querySelector('.modal__close');
- const closeModal = document.querySelector('body');
- const switchModal = () => {
-  modal.classList.toggle('modal--visible');
- }
- modalBtn.forEach(element => {
-  element.addEventListener('click', switchModal)
- });
- closeBtn.addEventListener('click', switchModal);
-}); */
-
 $(document).ready(function () {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
@@ -83,7 +69,7 @@ $(document).ready(function () {
                     $(form)[0].reset();
                     modal.removeClass('modal--visible');
                 },
-                error: function (response){
+                error: function (response) {
                     console.error('Ошибка запроса');
                 }
             });
@@ -92,6 +78,20 @@ $(document).ready(function () {
 
     /* Маска для телефона */
     $('[type=tel]').mask('+7(000) 00-00-000', { placeholder: "+7 (___)__-__-___" });
+   /*  var player;
+    $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '465',
+            width: '100%',
+            videoId: 'o5_cr1EagFM',
+            events: {
+                'onReady': videoPlay,
+            }
+        });
+    })
+    function videoPlay(event) {
+        event.target.playVideo();
+    } */
 
     /* создание yandex карты  */
     ymaps.ready(function () {
@@ -101,28 +101,19 @@ $(document).ready(function () {
         }, {
             searchControlProvider: 'yandex#search'
         }),
-
             // Создаём макет содержимого.
             MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
                 '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
             ),
-
             myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
                 hintContent: 'Собственный значок метки',
                 balloonContent: 'Это красивая метка'
             }, {
-                // Опции.
-                // Необходимо указать данный тип макета.
-                iconLayout: 'default#image',
                 // Своё изображение иконки метки.
                 iconImageHref: 'img/location.png',
                 // Размеры метки.
                 iconImageSize: [32, 32],
-                // Смещение левого верхнего угла иконки относительно
-                // её "ножки" (точки привязки).
-                iconImageOffset: [-5, -38]
             });
-
         myMap.geoObjects
             .add(myPlacemark)
     });
